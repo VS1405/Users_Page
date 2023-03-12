c4817e9a5f895ab490617d54479bf6a5611e11b1
Varsha B. Mhaske <varshamhaske808@gmail.com>
2023-03-12 18:05:49 +0530

    React Portal added

--- ErrorModal.js
+++ ErrorModal.js
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import  ReactDOM  from 'react-dom';
 import Card from './Card'
 import Button from './Button'
 import classes from './ErrorModal.module.css'
 
 const ErrorModal = props => {
-    return (
-        <div>
-            <div className={classes.backdrop} onClick={props.onConfirm}/>
+    const BreakDown = props => {
+        return (
+            <div className={classes.backdrop} onClick={props.onConfirm} />
+        )
+    }
+    const ModalOverLay = props => {
+        return (
             <Card className={classes.modal}>
                 <header>
                     <h2>{props.title}</h2>
@@ -18,7 +23,34 @@ const ErrorModal = props => {
                     <Button className={classes.button} onClick={props.onConfirm}>Okay</Button>
                 </footer>
             </Card>
-        </div>
+        )
+    }
+    return (
+        <React.Fragment>
+            {/* <div className={classes.backdrop} onClick={props.onConfirm}/> */}
+            {/* <Card className={classes.modal}>
+                <header>
+                    <h2>{props.title}</h2>
+                </header>
+                <div className={classes.content}>
+                    <p>{props.msg}</p>
+                </div>
+                <footer>
+                    <Button className={classes.button} onClick={props.onConfirm}>Okay</Button>
+                </footer>
+            </Card> */}
+            {ReactDOM.createPortal(<BreakDown onClick={props.onConfirm} />,
+                document.getElementById('backdrop-root'))
+            }
+            {ReactDOM.createPortal(
+                <ModalOverLay
+                    title={props.title}
+                     msg={props.msg} 
+                     onConfirm={props.onConfirm}
+                />,
+                document.getElementById('overLay-root'))
+            }
+        </React.Fragment>
     )
 }
 export default ErrorModal;
